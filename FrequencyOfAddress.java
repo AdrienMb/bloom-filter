cdebfee82b4d3faf610ab6351a8c0e900cab2f78
Adrien Mbirko <adrien.mbirko@gmail.com>
2018-05-28 22:06:54 +0200

    Menu + hashmap instead of Ip class

--- FrequencyOfAddress.java
+++ FrequencyOfAddress.java
@@ -1,28 +1,23 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FrequencyOfAddress {
 
-	public static List<IP> findFrequencies(List<String> ips){
-		List<IP> result = new ArrayList<IP>();
+	public static Map<String,Integer> findFrequencies(List<String> ips){
+		Map<String,Integer> result = new HashMap<String,Integer>();
 		String ip;
 		boolean notFound=true;
 		for(int i=0;i<ips.size();i++){
 			ip = ips.get(i);
-			for(int j=0;j<result.size();j++){
-				if(ip.equals(result.get(j).getAddress())){
-					IP newFrequency=result.get(j);
-					newFrequency.setFrequency(newFrequency.getFrequency()+1);
-					result.set(j, newFrequency);
-					notFound=false;
-					break;
-				}
+			if (result.containsKey(ip)) {
+				int count = result.get(ip);
+				count++;
+				result.put(ip,count);
+			} else {
+				result.put(ip,1);
 			}
-			if(notFound){
-				IP newIp=new IP(ip);
-				result.add(newIp);
-			}
-			notFound=true;
 		}
 		return result;
 	}
--- Test.java
+++ Test.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Test {
 
@@ -7,25 +9,55 @@ public class Test {
 		// On ouvre notre fichier et place les ips dans une liste
 		List<String> ips = OpenFile.openFile("C:/Users/adrie/Documents/workspace-Algo/Traces-IP-Projet.csv");
 		System.out.println("Nombre d'ips : "+ips.size());
-		
-		System.out.println("\nAvec un comptage exact");
-		
-		//On effectue un comptage exact
-		List<IP> result = FrequencyOfAddress.findFrequencies(ips);
-		// On affiche une partie des résultats pour vérifier
-		for(int i=0;i<5;i++){	
-			System.out.println("IP : "+result.get(i).getAddress()+" - Fréquence : "+result.get(i).getFrequency());
+		boolean exit = false;
+		Scanner scanner = new Scanner(System.in);
+		while(!exit) {
+			System.out.println("\nChoix :");
+			System.out.println("1 : pour effectuer un comptage exact");
+			System.out.println("2 : pour utiliser le filtre de bloom");
+			System.out.println("3 : pour quitter");
+			int choice = scanner.nextInt();
+			switch (choice) {
+			case 1:
+				//On effectue un comptage exact
+				System.out.println("\nAvec un comptage exact");
+				long startTime = System.currentTimeMillis();
+				Map<String,Integer> result = FrequencyOfAddress.findFrequencies(ips);
+				long endTime = System.currentTimeMillis();
+				// On affiche une partie des résultats pour vérifier
+				for(int i=0;i<600;i+=100){	
+					System.out.println("IP : "+ips.get(i)+" - Fréquence : "+result.get(ips.get(i)));
+				}
+				System.out.println(endTime-startTime+"ms");
+				break;
+
+			case 2:
+				//Avec le filtre de bloom cette fois
+				int tailleBloom=3354771;
+				int nbHash=23;
+				System.out.println("\nAvec filtre de bloom de taille "+tailleBloom+" avec "+nbHash+" fonctions de hashage");
+				startTime = System.currentTimeMillis();
+				Bloom b = new Bloom(tailleBloom,nbHash);
+				b.initBloom(ips);
+				endTime = System.currentTimeMillis();
+				// On affiche une partie des résultats pour vérifier
+				for(int i=0;i<600;i+=100){	
+					System.out.println("IP : "+ips.get(i)+" - Fréquence : "+b.countIp(ips.get(i)));
+				}
+				System.out.println(endTime-startTime+"ms");
+				break;
+			case 3:
+				exit=true;
+				break;
+			default:
+				exit=true;
+				break;
+			}
 		}
-		
-		System.out.println("\nAvec filtre de bloom");
-		
-		//Avec le filtre de bloom cette fois
-		Bloom b = new Bloom(3354771,23);
-		b.initBloom(ips);
-		// On affiche une partie des résultats pour vérifier
-		for(int i=0;i<5;i++){	
-			System.out.println("IP : "+result.get(i).getAddress()+" - Fréquence : "+b.countIp(result.get(i).getAddress()));
-		}
+
+
+
+
 	}
 
 }
